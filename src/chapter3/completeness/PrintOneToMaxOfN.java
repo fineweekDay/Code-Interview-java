ca07dbfc4531cfffab30c7e8aab62ca612b58b6e
yuy <yyy14314408@163.com>
2017-08-11 22:22:22 +0800

    add PrintOneToMaxOfN

--- src/chapter3/completeness/PrintOneToMaxOfN.java
+++ src/chapter3/completeness/PrintOneToMaxOfN.java
@@ -6,17 +6,58 @@ package chapter3.completeness;
  */
 public class PrintOneToMaxOfN {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        int[] number=new int[3];
+        try {
+            while(!increment(number)){
+                printNumber(number);
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    //方法：数字的全排列 + 递归
-    public static void printMax(int N){
-        if(N<=0)
-            return;
+    //剑指OFFER解法
+    public static boolean increment(int[] number) throws Exception {
+        if(number.length==0 ||number==null)
+            throw new Exception();
 
+        boolean isOverFlow=false;
+        int carry=0;
 
+        for(int i=number.length-1;i>=0;i--){
+            int nSum=number[i]+carry;
+            if(i==number.length-1)
+                nSum++;
 
+            if(nSum>=10){
+                if(i==0)
+                    isOverFlow=true;
+                else{
+                    nSum-=10;
+                    carry=1;
+                    number[i]=nSum;
+                }
+            }else{
+                number[i]=nSum;
+                break;
+            }
+        }
 
+        return isOverFlow;
+    }
+
+    public static void printNumber(int[] number){
+        boolean isBeginning=true;
+
+//        System.out.println(Arrays.toString(number));
+
+        for(int i=0;i<number.length;i++){
+            if(isBeginning && number[i]!=0)
+                isBeginning=false;
+            if(!isBeginning)
+                System.out.print(number[i]);
+        }
     }
 }
