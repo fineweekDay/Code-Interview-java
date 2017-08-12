2a7b65f5d1f2e9ea1aa8c1b3c9f3dcc7712a7ab6
yuy <yyy14314408@163.com>
2017-08-12 15:08:15 +0800

    add DeleteNode

---
+++ src/chapter3/completeness/DeleteNode.java
@@ -0,0 +1,61 @@
+package chapter3.completeness;
+
+/**
+ * 题目：在o(1)时间删除链表节点
+ * Created by Administrator on 2017/8/11.
+ */
+public class DeleteNode {
+
+    static class ListNode{
+        int value;
+        ListNode next;
+
+        ListNode(int val,ListNode listNode){
+            this.value = val;
+            next=listNode;
+        }
+    }
+
+    //这种方法成立的基础是必须时被删除的节点在链表内
+    public static ListNode delete(ListNode head,ListNode toBeDelete){
+        if(head==null || toBeDelete==null)
+            return null;
+
+        if(toBeDelete.next != null){
+            //删除的不是尾节点
+            ListNode pNext=toBeDelete.next;
+            toBeDelete.next=pNext.next;
+            toBeDelete.value=pNext.value;
+
+            pNext=null;
+        }else if(head==toBeDelete){
+            //仅有一个节点
+            head=null;
+            toBeDelete=null;
+        }else{
+            //链表中有多个节点，删除尾节点
+            ListNode pNode=head;
+            while(pNode.next!=toBeDelete){
+                pNode=pNode.next;
+            }
+            pNode.next=null;
+            toBeDelete=null;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args){
+        ListNode tail=new ListNode(1,null);
+        ListNode c=new ListNode(2,tail);
+        ListNode b=new ListNode(3,c);
+        ListNode head=new ListNode(4,b);
+//        ListNode head=tail;
+        head=delete( head,c);
+        while(head!=null){
+            System.out.println(head.value);
+            head=head.next;
+        }
+    }
+
+}
